60dfefa971f482a0a64690ea1218f6b546173d19
M.AhsenRiaz <ahsenriaz2001@gmail.com>
2024-04-01 15:28:36 +0500

    feat: set up pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
+import './assets/main.css'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(createPinia())
 app.mount('#app')
