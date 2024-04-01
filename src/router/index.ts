ba2af859f86c67c5e717c6028efe2de8f8594db8
M.AhsenRiaz <ahsenriaz2001@gmail.com>
2024-04-01 15:27:39 +0500

    feat: handle private routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,29 +2,48 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Login from '../views/Auth/Login.vue'
 import Register from '../views/Auth/Register.vue'
+import { isUserLoggedIn } from '@/lib/utils'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes:[
+    routes: [
         {
-            path:'/',
-            name:'home',
-            component:HomeView,
-            // meta:{
-            //     auth:true
-            // }
+            path: '/',
+            name: 'home',
+            component: HomeView,
+            meta:{
+                auth:true
+            }
         },
         {
-            path:'/auth/login',
-            name:'auth-login',
-            component:Login,
+            path: '/auth/login',
+            name: 'auth-login',
+            component: Login,
         },
         {
-            path:'/auth/register',
-            name:'auth-register',
-            component:Register,
+            path: '/auth/register',
+            name: 'auth-register',
+            component: Register,
         }
     ]
+})
+
+router.beforeEach(async (to, from, next) => {
+    const isLoggedIn = await isUserLoggedIn();
+
+    if (to.name !== 'auth-login' && !isLoggedIn) {
+        if (!to.meta.auth) {
+            return next()
+        }
+
+        return next({ name: 'auth-login' })
+    }
+    else if (to.name === 'auth-login' || to.name ==='auth-register' && isLoggedIn) {
+        return next({ name: 'home' })
+    }
+    else {
+        next()
+    }
 })
 
 export default router   
